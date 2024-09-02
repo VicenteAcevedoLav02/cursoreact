93d6d72c016e280d47a1365c42d3e91637fb4a57
Vicente Acevedo <vacevedo1@miuandes.cl>
2024-09-01 23:57:15 -0400

    Estudio 6: página 57

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,56 +1,73 @@
 import * as React from 'react';
 
-const list = [
-  {
+const App = () => {
+    
+  const stories = [
+    {
       title: 'React',
-      url: 'https://puppies.com/',
+      url: 'https://reactjs.org/',
       author: 'Jordan Walke',
       num_comments: 3,
       points: 4,
       objectID: 0,
-  },
-  {
-      title: 'Reduxs',
+    },
+    {
+      title: 'Redux',
       url: 'https://redux.js.org/',
       author: 'Dan Abramov, Andrew Clark',
       num_comments: 2,
       points: 5,
       objectID: 1,
-  },
-];
-
-
-/*Esto es el hola mundog de los pobres */
-function App() {
-
-  function getTitle(title){
-    return title;
-  }
+    },
+  ];
 
   return (
     <div>
-      <h1> My Hacker Stories </h1>
+      <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <Search />
 
       <hr />
-      <ul>
-        {list.map(function (item) {
-          return (
-            <li key={item.objectID}>
-            <span>
-            <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            </li>
-          );
-        
-        })}
-      </ul>
+
+      <List list={stories} />
     </div>
   );
 }
+
+const Search = () => {
+  const handleChange = (event) => {
+    // synthetic event
+    console.log(event);
+    // value of target (here: input HTML element)
+    console.log(event.target.value);
+    };
+    
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+    </div>
+  );
+};
+
+
+const List = (props) => (
+  <ul>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+);
+
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
+);
+
 export default App;
